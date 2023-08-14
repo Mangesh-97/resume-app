c7f3c0fae90289017de54de5c1d58c6f00e2aa6f
Mangesh047 <dholem39@gmail.com>
2023-08-14 15:01:02 +0530

    mat toolbar and sidenav added

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+  @ViewChild('sidenav')
+  sidenav!: any
+  ngOnInit(): void {
+  }
+
+  
+
+
+}
